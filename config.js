c84b34e3f6a32dd70e7f0664b79ffb3577761f6f
ceebotarg <ceebotarg@gmail.com>
2025-07-05 15:26:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ const { existsSync } = require('fs')
 if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'kord_ai-CISwIgERff8Q8eyd',
+    SESSION_ID: process.env.SESSION_ID || 'kkord_ai-CT1p0IUivXIemg9i',
     OWNER_NUMBER: process.env.OWNER_NUMBER || "2349043883888",
     WORKTYPE: process.env.WORKTYPE || "private",
-    PREFIX: process.env.PREFIX || "[.]",
+    PREFIX: process.env.PREFIX || "[]",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || true,
     MONGODB_URI: process.env.MONGODB_URI || "",
     STATUS_VIEW: process.env.STATUS_VIEW || true,
